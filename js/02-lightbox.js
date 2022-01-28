58d48b458a981b6a409631aa7588f60881820383
Alex Borets <alexborets2812@gmail.com>
2022-01-28 14:52:30 +0200

    Task 2 rdy

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__item" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}" 
+    alt="${description}" 
+    />
+    </a>
+    </div>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox(`.gallery a`, {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
